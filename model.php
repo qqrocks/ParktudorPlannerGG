5f67dd86724e306c407924d5f9ce15bdef344ff7
mekosten <mkosten@parktudor.org>
2015-05-08 12:37:17 -0400

    modelstuff

--- model.php
+++ model.php
@@ -1,5 +1,17 @@
 <?php
 function add_student($stuID, $fname, $lanme, $grade, $adID){
     global $db;
-    $query='INSERT INTO `students`(`studentID`, `s_f_name`, `s_l_name`, `gradeID`, `advisorID`) VALUES ($stuID, $fname, $lname, $grade, $adID)';
+    $query='INSERT INTO students(studentID, s_f_name, s_l_name, gradeID, advisorID) VALUES ($stuID, $fname, $lname, $grade, $adID)';
+    $db->exec($query);
+}
+function add_advisor($adID, $fname, $lname)
+{
+    global $db;
+    $query='INSERT into advisors(advisorID, a_f_name, a_l_name) VALUES ($adID, $fname, $lname)';
+    $db->exec($query);
+}
+
+function add_class($cID,$cName, $credID, $perID, $deptID, $required)
+{
+    global $db;
 }
