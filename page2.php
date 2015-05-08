cbcce0d275789b81ccb2b6d01ff9e771451f021b
Quinton Petrucciani <qqpetro@gmail.com>
2015-05-08 12:16:33 -0400

    margit

---
+++ page2.php
@@ -0,0 +1,36 @@
+<html >
+<head>
+
+    <title>The Park Tudor Planner</title>
+
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico">
+
+
+</head>
+
+<body >
+<center><img src= "logo.png" alt="some_text" style="width: 222px; height: 169px  "></center>
+<center><div id="logo" >
+
+        <div class="heady"><p>Welcome Advisors!</p></div>
+        <div class="sent">Please select the adivisory of the student you are looking for</div>
+        <table style="width:750px">
+            <tr>
+                <td>Name</td>
+                <td>Delete</td>
+                <td> Click to activate Delete </td>
+
+            </tr>
+
+
+            </tr>
+
+        </table>
+    </div>
+</center>
+
+
+</body>
+</html>
