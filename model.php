36936cfa37d1649f2dafca9e4b19f310b5c1f57b
mekosten <mkosten@parktudor.org>
2015-05-08 12:21:27 -0400

    fee

---
+++ model.php
@@ -0,0 +1,5 @@
+<?php
+function add_student($stuID, $fname, $lanme, $grade, $adID){
+    global $db;
+    $query='INSERT INTO `students`(`studentID`, `s_f_name`, `s_l_name`, `gradeID`, `advisorID`) VALUES ($stuID, $fname, $lname, $grade, $adID)';
+}
