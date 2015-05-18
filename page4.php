37967fe67af09e2a2834de272249f3489de2d5de
Quinton Petrucciani <qqpetro@gmail.com>
2015-05-18 11:48:11 -0400

    eadjiewjieji

---
+++ page4.php
@@ -0,0 +1,48 @@
+
+<html >
+<head>
+
+    <title>The Park Tudor Planner</title>
+
+
+    <link rel="stylesheet" type="text/css" href="page4.css">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico">
+
+
+</head>
+
+<body >
+<center><img src= "logo.png" alt="some_text" style="width: 222px; height: 169px  "></center>
+<center><div id="logo" >
+
+        <div class="heady"><p>freshman</p></div>
+        <table>
+            <tr>
+                <td>
+                    Credits Earned
+                </td>
+                <td class="special">
+                    marget php
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Free Periods
+                </td>
+                <td class="special">
+                    marget php
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Periods planned
+                </td>
+                <td class="special">
+                    marget php
+                </td>
+            </tr>
+        </table>
+
+    </div>
+</center>
+
