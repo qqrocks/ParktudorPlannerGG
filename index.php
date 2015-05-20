2d20d83f4f1ba49bc3c9b65ef44ecbc622a9eca6
mekosten <mkosten@parktudor.org>
2015-05-20 16:10:33 -0400

    YOU SKIPPED CLASS

--- index.php
+++ index.php
@@ -1,64 +1,16 @@
+<?php
+//require('/model_pages/advisor.php');
+//require('/model_pages/advisor_db.php');
+//require('/model_pages/course.php');
+//require('/model_pages/course_sel.php');
+//require('/model_pages/course_selection_db.php');
+//require('/model_pages/courses_db.php');
+//require('/model_pages/credit_db.php');
+//require('/model_pages/database.php');
 
-<html >
-<head>
-
-    <title>The Park Tudor Planner</title>
-
-
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico">
-
-
-</head>
-
-<body >
-<center><img src= "logo.png" alt="some_text" style="width: 222px; height: 169px  "></center>
-<center><div id="logo" >
-
-        <div class="heady"><p>Welcome Advisors!</p></div>
-        <div class="sent">Please select the adivisory of the student you are looking for</div>
-        <table style="width:750px">
-            <tr>
-                <td>Freshman</td>
-                <td>Sophmore</td>
-                <td> Junior </td>
-                <td> Senior </td>
-            </tr>
-
-            <tr>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-            </tr>
-            <tr>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-            </tr>
-            <tr>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-            </tr>
-            <tr>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-                <td>~</td>
-            </tr>
-            <tr></tr>
-        </table>
-        <a class="button" href="page2.php" target="_blank">Page 2</a>
-
-    </div>
-</center>
-
-
-
-
-
-</body>
-</html>
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'list_advisors';
---
+++ page1.php
@@ -0,0 +1,64 @@
+
+<html >
+<head>
+
+    <title>The Park Tudor Planner</title>
+
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico">
+
+
+</head>
+
+<body >
+<center><img src= "logo.png" alt="some_text" style="width: 222px; height: 169px  "></center>
+<center><div id="logo" >
+
+        <div class="heady"><p>Welcome Advisors!</p></div>
+        <div class="sent">Please select the adivisory of the student you are looking for</div>
+        <table style="width:750px">
+            <tr>
+                <td>Freshman</td>
+                <td>Sophmore</td>
+                <td> Junior </td>
+                <td> Senior </td>
+            </tr>
+
+            <tr>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+            </tr>
+            <tr>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+            </tr>
+            <tr>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+            </tr>
+            <tr>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+                <td>~</td>
+            </tr>
+            <tr></tr>
+        </table>
+        <a class="button" href="page2.php" target="_blank">Page 2</a>
+
+    </div>
+</center>
+
+
+
+
+
+</body>
+</html>
